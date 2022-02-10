a6a5223caaefbcd08ce0f4be00263dde74a109b4
GastyDev <91214872+GastDevelopment@users.noreply.github.com>
2022-02-10 16:16:26 +0000

    Patch #2
    Fixed rounding issue
    Fixed price not displaying issue
    Cleaned up code and added more comments

--- Application.py
+++ Application.py
@@ -10,6 +10,7 @@ from matplotlib.pyplot import text
 clientInfo = [[]]
 
 #Setup the window and it's properties
+global total
 total = 0.0
 window = tkinter.Tk()
 window.title("Gast's Outlet")
@@ -19,13 +20,19 @@ greetings = tkinter.Label(window, text="Welcome to Gast's Outlet!\nChoose an opt
                           background="moccasin", font=("Arial", 24))
 
 #Functions
+#Takeaway Function
 def takeawayFunction():
     t = tkinter.Tk()
     t.title("Gast's Takeaway")
     t.geometry("900x900")
     t.configure(background="moccasin")
+    address = tkinter.Entry(t, fg="Black", bg="white", width=15, font=("Arial", 12))
+    file = open("logs.txt", "w")
+    addy = address.get()
+    file.write("Address: " + addy)
 
-def pFunc():
+    file.close()
+    address.pack()
     
 #Menu Function
 def orderFunction():
@@ -35,11 +42,25 @@ def orderFunction():
     m.configure(background="moccasin")
     e = "Total £" + str(total)
 
-    pasta = tkinter.Button(m, text="Pasta             £9.99", fg="Black", bg="cyan2", background="moccasin", command=,height=2, width=20,font=("Arial", 15))
-    burger = tkinter.Button(m, text="Burger           £29.99", fg="Black", bg="cyan2", background="moccasin", command=,height=2, width=20,font=("Arial", 15))
-    pizza = tkinter.Button(m, text="Pizza             £39.99", fg="Black", bg="cyan2", background="moccasin", command=,height=2, width=20,font=("Arial", 15))
+    #Functions for the buttons
+    def pastaFunc():
+        global total
+        total += 9.99 
+        bill.configure(text="Total £" + str(round(total, 2)))
+    def burgerFunc():
+        global total
+        total += 29.99
+        bill.configure(text="Total £" + str(round(total, 2)))
+    def pizzaFunc():
+        global total
+        total += 39.99
+        bill.configure(text="Total £" + str(round(total, 2)))
 
-    bill = tkinter.Label(m, textvariable=e, fg="black", background="moccasin", font=("Arial", 20))
+    #Buttons and packing everything in the function
+    pasta = tkinter.Button(m, text="Pasta             £9.99", fg="Black", bg="cyan2", background="moccasin", command=pastaFunc,height=2, width=20,font=("Arial", 15))
+    burger = tkinter.Button(m, text="Burger           £29.99", fg="Black", bg="cyan2", background="moccasin", command=burgerFunc,height=2, width=20,font=("Arial", 15))
+    pizza = tkinter.Button(m, text="Pizza             £39.99", fg="Black", bg="cyan2", background="moccasin", command=pizzaFunc,height=2, width=20,font=("Arial", 15))
+    bill = tkinter.Label(m, text=e, fg="black", background="moccasin", font=("Arial", 20))
     bill.pack()
     burger.pack()
     pizza.pack()
@@ -47,8 +68,8 @@ def orderFunction():
     
 
 #Buttons
-takeaway = tkinter.Button(window, font=("Arial", 30), text="Tip", fg="Black", bg="cyan2", command=takeawayFunction, height=3, width=8)
-order = tkinter.Button(window,font=("Arial", 30), text="Order", fg="Black", bg="cyan2", command=orderFunction, height=2, width=5)
+takeaway = tkinter.Button(window, font=("Arial", 30), text="Takeaway", fg="Black", bg="cyan2", command=takeawayFunction, height=2, width=8)
+order = tkinter.Button(window,font=("Arial", 30), text="Order", fg="Black", bg="cyan2", command=orderFunction, height=2, width=8)
 
 #Pack everything here
 greetings.pack()
